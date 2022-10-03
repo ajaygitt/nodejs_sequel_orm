c6c68868b8cecc5689561a1a75c8b6f8d51c5ccb
ajaypradeep <ajaypradeep1712@gmail.com>
2022-10-03 20:30:02 +0530

    insertion completed

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
-const express=require("express")
-const app=express();
-const dotenv=require("dotenv")
+const express = require("express")
+const app = express();
+const dotenv = require("dotenv")
 dotenv.config();
-const sequelize=require("./utils/db")
-const userModel=require("./models/user")
-const SequelizeLib=require("sequelize")
-const PORT=8000;
+const sequelize = require("./utils/db")
+const userModel = require("./models/user")
+const SequelizeLib = require("sequelize");
+const User = require("./models/user");
+const PORT = 8000;
 
 
 
 // convert sequelize to actual sql tables
 
 
-sequelize.sync().then((result)=>{
-    console.log("result",result)
-}).catch((err)=>{
-    console.log("this is an error",err)
+sequelize.sync().then((result) => {
+    console.log("result", result)
+}).catch((err) => {
+    console.log("this is an error", err)
 })
 
 
 
-app.listen(PORT,()=>{
+// route for insert data to user table
 
-    console.log("server started on "+PORT)
+app.get("/create", async (req, res) => {
+
+    try {
+        const { email, name } = req.body;
+
+        // insert to table
+        await User.create({ name: "test", age: 22 })
+
+        res.status(200).json({ success: true })
+
+    }
+    catch (err) {
+        res.status(400).json({ error: err })
+    }
+
+
+
+
+
+
+
+})
+
+app.listen(PORT, () => {
+
+    console.log("server started on " + PORT)
 })
 
